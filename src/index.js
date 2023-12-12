24d0d801647814e87f64c4adba71dbc2c9c297e9
AlexBinkovskyy <alex_bin@ukr.net>
2023-12-13 01:35:29 +0200

    aaa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import axios from '../node_modules/axios';
 import Notiflix from '../node_modules/notiflix';
 import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 Notiflix.Notify.init({
   width: '500px',
   position: 'center-top',
@@ -10,37 +11,73 @@ Notiflix.Notify.init({
   timeout: 3500,
 });
 
+let options = {
+  root: null,
+  rootMargin: '250px',
+};
+
+let observer = new IntersectionObserver(onLastItem, options);
+let queryString = '';
+let page = 1;
+
 const MAIN_URL = 'https://pixabay.com/api/';
 const KEY = '29244852-b91e3d5198a9840c92a9dad06';
 const galleryItem = document.querySelector('.gallery');
+const upperBtn = document.querySelector('.upper');
+// upperBtn.addEventListener('click', onclick);
+const target = document.querySelector('.js-guard');
 
 const form = document.querySelector('#search-form');
 form.addEventListener('submit', onSubmit);
 
+function onLastItem(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      fetchQuery(queryString)
+        .then(response => {
+          // gallery.refresh();
+          addLayout(response);
+        })
+        .catch(err => console.log(err));
+    }
+  });
+}
+
+function smoothScroll(galleryItem, scrollPercentage) {
+  const { height: cardHeight } =
+    galleryItem.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * scrollPercentage,
+    behavior: 'smooth',
+  });
+}
+
 async function onSubmit(event) {
   event.preventDefault();
-  const queryString = event.target.searchQuery.value
+  galleryItem.innerHTML = '';
+  queryString = event.target.searchQuery.value
     .trim()
     .toLowerCase()
     .replaceAll(' ', '+');
-  // form.reset();
 
   await fetchQuery(queryString)
     .then(response => {
+      console.log(response);
       addLayout(response);
     })
     .catch(err => console.log(err));
 }
+
 async function fetchQuery(queryString) {
   const params = {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
     per_page: 40,
-    page: 1,
   };
   const axInstance = await axios(
-    `${MAIN_URL}?key=${KEY}&q=${queryString}&image_type=${params.image_type}&orientation=${params.orientation}&safesearch=${params.safesearch}&per_page=${params.per_page}&page=${params.page}`
+    `${MAIN_URL}?key=${KEY}&q=${queryString}&image_type=${params.image_type}&orientation=${params.orientation}&safesearch=${params.safesearch}&per_page=${params.per_page}&page=${page}`
   );
   return axInstance;
 }
@@ -85,14 +122,18 @@ function createLayout({ hits }) {
     })
     .join('');
 
-  galleryItem.innerHTML = res;
-
-  const { height: cardHeight } =
-    galleryItem.firstElementChild.getBoundingClientRect();
-  window.scrollBy({
-    top: cardHeight * 0.25,
-    behavior: 'smooth',
-  });
-
+  galleryItem.insertAdjacentHTML('beforeend', res);
   let gallery = new SimpleLightbox('.gallery a');
+
+  smoothScroll(galleryItem, 0.25);
+
+  observer.observe(target);
+
+  upperBtn.removeAttribute('disabled');
+  upperBtn.style.fillOpacity = 1;
+  upperBtn.style.cursor = 'pointer';
+
+  upperBtn.onclick = () => {
+    window.scrollTo(0, 0);
+  };
 }
